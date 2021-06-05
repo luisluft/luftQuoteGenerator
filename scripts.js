7a16621cb8ff514166b459db378ed8f2c3d6f539
luisluft <luberluft@gmail.com>
2021-06-05 14:42:22 -0400

    add loading feature

--- scripts.js
+++ scripts.js
@@ -3,23 +3,35 @@ const quoteText = document.getElementById("quote-text");
 const authorText = document.getElementById("author-text");
 const twitterButton = document.getElementById("twitter-button");
 const newQuoteButton = document.getElementById("new-quote-button");
-
+const loader = document.getElementById("loader");
 let apiQuotes = [];
 
+function showLoading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+function hideLoading() {
+  loader.hidden = true;
+  quoteContainer.hidden = false;
+}
+
 function showNewQuote() {
+  showLoading();
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
   if (!quote.author) authorText.textContent = "Unknown";
+  if (quote.text.length > 100) quoteText.classList.add("long-quote");
+  else quoteText.classList.remove("long-quote");
 
   authorText.textContent = quote.author;
   quoteText.textContent = quote.text;
-
-  if (quote.text.length > 100) quoteText.classList.add("long-quote");
-  else quoteText.classList.remove("long-quote");
+  hideLoading();
 }
 
 // Get quotes from the API
 async function getQuotes() {
+  showLoading();
   const apiUrl = "https://type.fit/api/quotes";
   try {
     const response = await fetch(apiUrl);
