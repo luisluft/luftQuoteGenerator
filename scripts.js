3ed54e5155b47e7e5d097e68b19c0a4dbda84e18
luisluft <luberluft@gmail.com>
2021-06-05 14:34:21 -0400

    add functionality to new quote and twitter button

--- scripts.js
+++ scripts.js
@@ -1,8 +1,21 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote-text");
+const authorText = document.getElementById("author-text");
+const twitterButton = document.getElementById("twitter-button");
+const newQuoteButton = document.getElementById("new-quote-button");
+
 let apiQuotes = [];
 
 function showNewQuote() {
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  console.log(quote);
+
+  if (!quote.author) authorText.textContent = "Unknown";
+
+  authorText.textContent = quote.author;
+  quoteText.textContent = quote.text;
+
+  if (quote.text.length > 100) quoteText.classList.add("long-quote");
+  else quoteText.classList.remove("long-quote");
 }
 
 // Get quotes from the API
@@ -16,6 +29,16 @@ async function getQuotes() {
     alert(error);
   }
 }
+
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+
+  window.open(twitterUrl, "_blank");
+}
+
+// Event listeners
+newQuoteButton.addEventListener("click", showNewQuote);
+twitterButton.addEventListener("click", tweetQuote);
 
 // On Load
 getQuotes();
